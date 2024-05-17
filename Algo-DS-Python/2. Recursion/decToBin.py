a678cc118a37d085c8d9920b6b94054d29ec6441
forhadx <forhadsh1@gmail.com>
2024-05-17 14:53:57 +0600

    remove some files

--- Algo-DS-Python/2. Recursion/decToBin.py
+++ Algo-DS-Python/2. Recursion/decToBin.py
@@ -8,7 +8,7 @@ def decimalToBinary(n):
     if n== 0:
         return 0
     else:
-        return decimalToBinary(int(n/2)) * 10 + + (n%2)                       
+        return decimalToBinary(int(n/2)) * 10 + (n%2)                       
 
 
 print(decimalToBinary(13)) #1101
